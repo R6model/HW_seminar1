4fa71d3f62b58d3dd619ef5816728ebd2ec51ac9
Roman Markov <markovro@mail.ru>
2023-10-12 11:55:48 +0300

    Добавил Задачу 27

--- Task025/Program.cs
+++ Task025/Program.cs
@@ -2,10 +2,27 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-
+int Exponent(int osn, int exp)
+{
+    int result = 1;
+    for (int i = 0; i < exp; i++)
+        {
+            checked
+            {
+                result *= osn;
+            }
+            
+        }
+    return result;
+}
 
 Console.WriteLine("Введите число А");
 int osn = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите натуральное число B");
 int exp = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine()
+if (exp < 0)
+{
+    Console.WriteLine("Показатель степени не может быть отрицательным, повторите ввод");
+    return;
+}
+Console.WriteLine($"Число {osn} в степени {exp} равно {Exponent(osn, exp)}");
---
+++ Task027/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int SumNumbers(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    return sum;
+}
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = SumNumbers(number);
+Console.WriteLine($"Сумма цифр в числе {number} ->{result}");
