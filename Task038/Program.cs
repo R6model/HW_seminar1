4a23cd417fdfdc3d5677c1380f989326dbf547ab
Roman Markov <markovro@mail.ru>
2023-10-14 22:30:17 +0300

    Добавил задачу 38

---
+++ Task038/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+
+double[] CreateArrayRndDouble(int size, double min, double max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+void PrintArray(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F2}; ");
+        else Console.Write($"{arr[i]:F2}");
+    }
+    Console.WriteLine("]");
+}
+
+double DifferenceNumbers(double[] arr)
+{
+    double min = 0;
+    double max = 0;
+    double dif;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        min = arr[i];
+        if (arr[i] <= min) min = arr[i];
+        if (arr[i]>max)max=arr[i];                   
+    }
+    dif = max - min;
+    return dif;
+    
+}
+
+
+
+
+
+double[] array = CreateArrayRndDouble(4, 1, 8);
+PrintArray(array);
+double difference = DifferenceNumbers(array);
+Console.Write($"->{difference:F2}");
